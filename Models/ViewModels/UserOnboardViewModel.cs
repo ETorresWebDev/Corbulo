a0a1e712d028cdf6317a99800805524f3456100b
ETorresWebDev <colmonstrosity@gmail.com>
2016-01-19 21:54:31 -0800

    Update UserOnboardViewModel.cs

--- Models/ViewModels/UserOnboardViewModel.cs
+++ Models/ViewModels/UserOnboardViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Sabio.Web.Models.ViewModels
+namespace Corbulo.Web.Models.ViewModels
 {
     public class UserOnboardViewModel : BaseViewModel
     {
