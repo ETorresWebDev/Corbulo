5ef71117b747296a99fe0a1ccf76c5a3ece88fef
ETorresWebDev <colmonstrosity@gmail.com>
2016-01-19 21:56:44 -0800

    Update Corbulo.Services.Courses.js

--- Scripts/Corbulo.Services.Courses.js
+++ Scripts/Corbulo.Services.Courses.js
@@ -1,11 +1,11 @@
 ﻿//Seperation of concerns
-if (!sabio.services.courses) {
-    sabio.services.courses = {}
+if (!corbulo.services.courses) {
+    corbulo.services.courses = {}
 }
 
 //Calls from Index View
 //AJAX Post Call
-sabio.services.courses.add = function (courseData, onSuccess, onError) {
+corbulo.services.courses.add = function (courseData, onSuccess, onError) {
 
     var url = "/api/courses";
     //var myData = $("#courses-input-form").serialize();
@@ -25,7 +25,7 @@ sabio.services.courses.add = function (courseData, onSuccess, onError) {
 
 
 //AJAX Update Call
-sabio.services.courses.update = function (id, courseData, onSuccess, onError) {
+corbulo.services.courses.update = function (id, courseData, onSuccess, onError) {
     console.log(courseData);
     //var id = $("#id").val();
     var url = "/api/courses/" + id;
@@ -47,7 +47,7 @@ sabio.services.courses.update = function (id, courseData, onSuccess, onError) {
 
 
 //AJAX Get Call
-sabio.services.courses.get = function (id, onSuccess, onError) {
+corbulo.services.courses.get = function (id, onSuccess, onError) {
 
     //var id = $("#id").val();
     var url = "/api/courses/" + id;
@@ -67,7 +67,7 @@ sabio.services.courses.get = function (id, onSuccess, onError) {
 
 
 //AJAX Delete Call
-sabio.services.courses.delete = function (id, onSuccess, onError) {
+corbulo.services.courses.delete = function (id, onSuccess, onError) {
 
     //var id = $("#id").val();
     var url = "/api/courses/" + id;
@@ -86,7 +86,7 @@ sabio.services.courses.delete = function (id, onSuccess, onError) {
 
 //AJAX calls from list view
 //AJAX Get all Call
-sabio.services.courses.getAll = function (onSuccess, onError) {
+corbulo.services.courses.getAll = function (onSuccess, onError) {
 
     var url = "/api/Courses/";
     var settings = {
