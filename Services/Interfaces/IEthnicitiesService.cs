4a8f1668629617c098f672d19798a568ebd43403
ETorresWebDev <colmonstrosity@gmail.com>
2016-01-19 22:01:31 -0800

    Update IEthnicitiesService.cs

--- Services/Interfaces/IEthnicitiesService.cs
+++ Services/Interfaces/IEthnicitiesService.cs
@@ -1,10 +1,10 @@
-﻿using Sabio.Web.Domain;
+﻿using Corbulo.Web.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Sabio.Web.Services.Interfaces
+namespace Corbulo.Web.Services.Interfaces
 {
     public interface IEthnicitiesService
     {
