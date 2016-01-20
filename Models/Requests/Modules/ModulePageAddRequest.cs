38d59ed898c643d5cb365b2918ed4b74b4ec9614
ETorresWebDev <colmonstrosity@gmail.com>
2016-01-19 21:51:45 -0800

    Update ModulePageAddRequest.cs

--- Models/Requests/Modules/ModulePageAddRequest.cs
+++ Models/Requests/Modules/ModulePageAddRequest.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations;
 
-namespace Sabio.Web.Models.Requests
+namespace Corbulo.Web.Models.Requests
 {
     public class ModulePageAddRequest
     {                
