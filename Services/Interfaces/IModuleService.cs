e80a2d12d4209fa31218a8fed8780c8e1a77f2dc
ETorresWebDev <colmonstrosity@gmail.com>
2016-01-19 22:02:45 -0800

    Update IModuleService.cs

--- Services/Interfaces/IModuleService.cs
+++ Services/Interfaces/IModuleService.cs
@@ -1,12 +1,12 @@
-﻿using Sabio.Web.Domain;
-using Sabio.Web.Models.Requests;
+﻿using Corbulo.Web.Domain;
+using Corbulo.Web.Models.Requests;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Web.Services.Interfaces
+namespace Corbulo.Web.Services.Interfaces
 {
     public interface IModuleService
     {
